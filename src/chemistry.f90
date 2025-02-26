0dc36c070899109edd46aa72fa908772196c1e1c
ctowery <colin.towery@colorado.edu>
2025-02-26 15:33:44 -0700

    stupid bug

--- src/chemistry.f90
+++ src/chemistry.f90
@@ -20,7 +20,7 @@ contains
         real :: b1, b2, b3, b4, b5, b6, b7
 
         associate (t => t); end associate
-        npts = size(y) / n
+        npts = size(y) / npts
 
         !$acc parallel loop private(dcdt, c)
         do ipt = 0, npts-1
