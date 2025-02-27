6aa3c60bb4a3f22f01ea94bef054dce431c99175
anpa5279 <anpa5279@colorado.edu>
2025-02-27 13:36:18 -0700

    fixed input file in chem_ode_miniapp code.

--- src/chem_ode_miniapp.f90
+++ src/chem_ode_miniapp.f90
@@ -9,7 +9,7 @@ program chem_ode_miniapp
     implicit none ! ------------------------------------------------------------
     ! subroutines for gpu
 
-    character(len=*), parameter :: input_file = "./test/user_inputs.nml"
+    character(len=*), parameter :: input_file = "./user_inputs.nml"
     character(len=128) :: save_name
         !! output filenames
     real :: dt_save = 1e99
